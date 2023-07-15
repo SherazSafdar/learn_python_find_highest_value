fcd291a2348d71ba90f52ddd19e750c111ef9c52
sherry648 <107458123+sherry648@users.noreply.github.com>
2023-07-15 14:13:21 +0500

    Update git second practice.py

--- git second practice.py
+++ git second practice.py
@@ -1,10 +1,10 @@
-def find_high_value(a,b):
+def find_highest_value(a,b):
     if a>b:
         return(a) 
     else:
         return(b)
 variable1 = int(input("Please enter first number : "))
 variable2 = int(input("Please enter second number : "))
-ans = find_high_value(variable1, variable2)
-print("return value is")
+ans = find_highest_value(variable1, variable2)
+print("highest value is")
 print(ans)            
